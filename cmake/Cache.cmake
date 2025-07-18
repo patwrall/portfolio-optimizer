54fa25fe0767c2300062bf9c77c857668e2003ea
Template Janitor <template.janitor@example.com>
2025-07-18 22:05:43 +0000

    Cleanup template and initialize repository

--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -1,5 +1,5 @@
 # Enable cache if available
-function(myproject_enable_cache)
+function(portfolio_optimizer_enable_cache)
   set(CACHE_OPTION
       "ccache"
       CACHE STRING "Compiler cache to be used")
